bb08d0d643d95748582d1672068006b447874842
poheniks <66043010+poheniks@users.noreply.github.com>
2025-10-03 19:36:45 -0700

    Add project files.

---
+++ SkipSplashScreenPlugin.cs
@@ -0,0 +1,16 @@
+﻿
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+
+namespace SkipSplashScreen
+{
+    [BepInPlugin("poh.bepinex.plugins.skipsplashscreen", "Skip Splash Screen", "1.0.0")]
+    public class SkipSplashScreenPlugin : BasePlugin
+    {
+        public override void Load()
+        {
+            Log.LogInfo("Skipping splash screens!");
+            SplashCards.showTime = float.MinValue;
+        }
+    }
+}
